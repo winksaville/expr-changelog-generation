de67f227f2e11d7f88c94bec56aa5a93abd11c42
Wink Saville <wink@saville.com>
2024-11-22 12:55:27 -0800

    wip: Attempt to get commit author
    The "correct" approach seems to be to implement "get_commit_with_sha1"
    from github and then use code like get_author which returns the
    author. But it always does the `fetch_pr_metadata` which is expensive:
    
     async fn get_author(
        author_map: &mut HashMap<String, String>,
        repo_commit: &RepoCommit,
        octocrab: &Octocrab,
        repo_owner: &str,
        repo_name: &str,
        merge_commit_sha: &str,
     ) -> String {
        let result = fetch_pr_metadata(octocrab, repo_owner, repo_name, merge_commit_sha).await;
        let email = if let Some(commit_author) = repo_commit.commit.author.clone() {
            commit_author.email
        } else {
            "".to_string()
        };
    
        // I find this obtuse, I wonder what would be clearer?
        let author = author_map.entry(email.clone()).or_insert_with(|| {
            result.map_or("".to_string(), |pr| {
                pr.user.map_or("".to_string(), |user| user.login)
            })
        });
    
        println!("get_author: author: {author} email: {email}");
    
        author.clone()
     }
    
    So I think what needs to happen use a commit from git2 to get a commits
    sha1 and use that sha1 to get the same commit from github and then use
    the author.user.login from this github commit as the "author" in the
    changelog. I started doing that with git_commit_with_sha1 and
    get_commit_author but it's not working yet.
    
    For the time being I'm going to explore using the github api more directly
    rather than octocrab use octocrate where I might be able to fetch
    commits from github directly using list_commits with a sha1 page=1 limit=1,
    but we'll see.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use octocrab::{
 use std::{
     collections::{HashMap, HashSet},
     env::args,
+    error::Error,
+    f32::consts::E,
     io,
     path::{Path, PathBuf},
 };
@@ -73,7 +75,7 @@ fn description(input: &str) -> &str {
     }
 }
 
-fn format_commit(commit: &Commit) -> String {
+fn _format_commit(commit: &Commit) -> String {
     let author = commit.author().name().unwrap_or("Unknown").to_string();
     let message = commit.message().unwrap_or("No commit message");
     let description = description(message);
@@ -87,14 +89,31 @@ async fn fetch_pr_metadata(
     merge_commit_sha: &str,
 ) -> Option<PullRequest> {
     // Return the PullRequest
-    octocrab
+    //octocrab
+    //    .repos(repo_owner, repo_name)
+    //    .list_pulls(merge_commit_sha.to_string())
+    //    .send()
+    //    .await
+    //    .ok()?
+    //    .into_iter()
+    //    .next()
+
+    let result = octocrab
         .repos(repo_owner, repo_name)
         .list_pulls(merge_commit_sha.to_string())
         .send()
-        .await
-        .ok()?
-        .into_iter()
-        .next()
+        .await;
+    match result {
+        Ok(prs) => {
+            let pr = prs.into_iter().next();
+            eprintln!("fetch_pr_metadata: pr: {pr:?}");
+            pr
+        }
+        Err(e) => {
+            eprintln!("fetch_pr_metadata: error: {e:?}");
+            None
+        }
+    }
 }
 
 async fn get_author(
@@ -111,6 +130,8 @@ async fn get_author(
     } else {
         "".to_string()
     };
+
+    // I find this obtuse, I wonder what would be clearer?
     let author = author_map.entry(email.clone()).or_insert_with(|| {
         result.map_or("".to_string(), |pr| {
             pr.user.map_or("".to_string(), |user| user.login)
@@ -120,6 +141,92 @@ async fn get_author(
     println!("get_author: author: {author} email: {email}");
 
     author.clone()
+}
+
+pub async fn get_commit_with_sha1<'a>(
+    octocrab: &'a Octocrab,
+    owner: &'a str,
+    repo: &'a str,
+    _sha1: &'a str,
+) -> Result<Commit<'a>, Box<dyn Error>> {
+    // Construct the URL for the specific commit
+    //let url = format!("/repos/{}/{}/commits/{}", owner, repo, sha1);
+
+    //// Fetch the JSON response from the GitHub API
+    //let response = octocrab::instance().repos(owner, repo).get().await?;
+
+    //let x = response.commits_url(owner, repo, sha1).get().await?;
+
+    //// Deserialize the response into the Commit structure
+    //let commit: Commit = serde_json::from_value(response)?;
+
+    let commits = octocrab.repos(owner, repo).list_commits().send().await?;
+    for commit in commits {
+        println!("get_commit_with_sha1: commit: {commit:?}");
+    }
+
+    Err("get_commit_with_sha1: not implemented".into())
+}
+
+async fn get_commit_author(
+    _author_map: &mut HashMap<String, String>,
+    _commit: &Commit<'_>,
+    _octocrab: &Octocrab,
+    _repo_owner: &str,
+    _repo_name: &str,
+    _commit_sha: &str,
+) -> Option<String> {
+    //    // Get the email
+    //    let email = commit.author().email()?.to_string();
+    //    eprintln!("get_author_commit: email: {email}");
+    //
+    //    // If the author is in the author_map return it
+    //    if let Some(author) = author_map.get(&email) {
+    //        println!("get_author_commit: found author: {author} email: {email}");
+    //        return Some(author.clone());
+    //    }
+    //
+    //    // Url of commit
+    //    let url = format!("/repos/{}/{}/commits/{}", repo_owner, repo_name, commit_sha);
+    //    let result = octocrab.get(url, None::<&()>).await;
+    //    match result {
+    //        Ok(response) => {
+    //            let commit = response.json::<RepoCommit>().await;
+    //            match commit {
+    //                Ok(commit) => {
+    //                    let author = get_author(
+    //                        author_map,
+    //                        &commit,
+    //                        octocrab,
+    //                        repo_owner,
+    //                        repo_name,
+    //                        commit_sha,
+    //                    )
+    //                    .await;
+    //                    Some(author)
+    //                }
+    //                Err(e) => {
+    //                    eprintln!("get_commit_author: error: {e:?}");
+    //                    None
+    //                }
+    //            }
+    //        }
+    //        Err(e) => {
+    //            eprintln!("get_commit_author: error: {e:?}");
+    //            None
+    //        }
+    //    }
+    //
+    //    //let author = commit.author().login;
+    //
+    //    //// Add this unknown author to the author_map so we don't have to fetch it again
+    //    //author_map.insert(email.clone(), author.clone());
+    //
+    //    //// Return the author
+    //    //println!("get_author_commit: inserted author: {author} email: {email}");
+    //    //Some(author.clone())
+
+    None
 }
 
 async fn process_commits(
@@ -140,6 +247,8 @@ async fn process_commits(
     for commit in get_commits(repo) {
         let oid = commit.id();
 
+        eprintln!("process_commits: commit: TOL {oid}");
+
         if let Some(tag) = tags.get(&oid) {
             if first_tag_shown {
                 output.push('\n');
@@ -155,17 +264,22 @@ async fn process_commits(
         }
 
         if commit.parent_count() > 1 {
+            eprintln!("process_commits: commit: parent_count > 1 {oid}");
             if let Some(octocrab) = &octocrab {
-                let merge_commit_sha = commit.id().to_string();
+                let merge_commit_sha = oid.to_string();
+                assert!(merge_commit_sha == oid.to_string());
+                eprintln!("process_commits: merge_commit_sha: {merge_commit_sha} == {oid}");
+
                 let pull_request =
                     fetch_pr_metadata(octocrab, repo_owner, repo_name, &merge_commit_sha).await;
                 if let Some(pr) = pull_request {
                     let pr_commits = get_pr_commits(octocrab, repo_owner, repo_name, &pr).await;
+                    eprintln!("process_commits: get_pr_commits {oid}");
 
                     match pr_commits.len() {
-                        0 => {
+                        len if len == 0 => {
                             // Add the PR to the output
-                            eprintln!("process_commits: ODD no commits for PR {}", pr.number);
+                            eprintln!("process_commits: pr_commits.len: {len}, odd no commits for pr.number: {}", pr.number);
 
                             let pr_url = pr.html_url.map(|url| url.to_string()).unwrap_or_default();
                             let pr_description =
@@ -175,7 +289,12 @@ async fn process_commits(
                                 pr.number, pr_url, pr_description
                             ));
                         }
-                        1 => {
+                        len if len == 1 => {
+                            eprintln!(
+                                "process_commits: pr_commits.len: {len} pr.number: {}",
+                                pr.number
+                            );
+
                             // Add the PR to the output
                             let a_pr_commit = &pr_commits[0];
                             let pr_number = pr.number;
@@ -192,7 +311,12 @@ async fn process_commits(
                             ));
                             skip_set.insert(a_pr_commit.sha.to_string());
                         }
-                        _ => {
+                        len => {
+                            eprintln!(
+                                "process_commits: pr_commits.len(): {len} pr.number: {}",
+                                pr.number
+                            );
+
                             let pr_url = pr.html_url.map(|url| url.to_string()).unwrap_or_default();
                             let pr_number = pr.number;
                             let pr_description =
@@ -232,24 +356,38 @@ async fn process_commits(
         } else if skip_set.contains(oid.to_string().as_str()) {
             println!("process_commits: skipping {}", oid);
         } else {
-            // Add the commit to the output
-            if let Some(email) = commit.author().email() {
-                if let Some(author) = author_map.get(email) {
-                    output.push_str(&format!(
-                        "    - {} @{author}\n",
-                        description(commit.message().unwrap())));
+            if let Some(octocrab) = &octocrab {
+                if let Ok(_x) =
+                    get_commit_with_sha1(octocrab, repo_owner, repo_name, &oid.to_string()).await
+                {
+                    eprintln!("ok");
                 } else {
-                    // TODO: Use get_author!!
-                    //println!("No author for commit.email(): {:?}", commit);
-                    let commit_str = format!("{}\n", format_commit(&commit));
-                    output.push_str(&commit_str);
-                };
+                    eprintln!("nada");
+                }
             } else {
-                // TODO: Use get_author!!
-                let commit_str = format!("{}\n", format_commit(&commit));
-                output.push_str(&commit_str);
-            };
+                eprintln!("no octocrab");
+            }
         }
+        //} else if let Some(author) = get_commit_author(
+        //    &mut author_map,
+        //    &commit,
+        //    octocrab.as_ref().unwrap(),
+        //    repo_owner,
+        //    repo_name,
+        //    &commit.id().to_string(),
+        //)
+        //.await
+        //{
+        //    output.push_str(&format!(
+        //        "    - {} @{author}\n",
+        //        description(commit.message().unwrap())
+        //    ));
+        //} else {
+        //    output.push_str(&format!(
+        //        "    - {} <no author>\n",
+        //        description(commit.message().unwrap())
+        //    ));
+        //}
     }
 
     output
@@ -298,7 +436,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Get the filename of the repo_directory handling "." and ".."
         repo_directory.file_name().unwrap().to_str().unwrap()
     };
-
+    eprintln!(
+        "repo_directory: {repo_directory:?} repo_owner: {repo_owner} repo_name: {repo_name} "
+    );
     // Open the repository and get the tags
     let repo = Repository::open(&repo_directory).expect("Failed to open repository");
     let tags = get_tags(&repo);
